760853ec92bfd04539bc2b509948137d4226167c
Aarya BC <aaryabc49@gmail.com>
2021-07-12 12:13:32 -0700

    Delete testIssueCreation.js

--- testIssueCreation.js
+++
@@ -1,8 +0,0 @@
-// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-const octokit = new Octokit({ auth: `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` });
-
-await octokit.request('POST /repos/{owner}/{repo}/issues', {
-  owner: 'AaryaBC',
-  repo: 'test-repo',
-  title: 'Test Issue'
-})
